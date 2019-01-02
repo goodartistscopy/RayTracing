223f8da165299305f6b693928573e7efde31b496
Christophe Dehais <christophe.dehais@fittingbox.com>
2019-01-02 16:11:00 +0100

    Modularize Camera class

---
+++ camera.cpp
@@ -0,0 +1,57 @@
+#include "camera.h"
+#include "vec3.h"
+#include "ray.h"
+#include "utils.h"
+
+Camera::Camera() { }
+
+Camera::Camera(float hfov, float aspect, float aperture, float focus_dist)
+{
+    setup(hfov, aspect);
+
+    look_at(Vec3(0.0), Vec3(0, 0, -1), Vec3(0, 1, 0));
+    set_lens(aperture, focus_dist);
+}    
+
+void Camera::setup(float hfov, float aspect)
+{
+    float theta = hfov * (M_PI/ 180.0);
+    half_width = fabs(tan(theta/2.0));
+    half_height = half_width / aspect;
+
+    update_internals();
+}
+
+void Camera::look_at(const Vec3 &from, const Vec3 &to, const Vec3 &up)
+{
+    origin = from;
+    cz = unit_vector(from - to);
+    cx = unit_vector(cross(up, cz));
+    cy = cross(cz, cx);
+
+    update_internals();
+}
+
+void Camera::set_lens(float aperture, float _focus_dist)
+{
+    lens_radius = aperture / 2.0;
+    focus_dist = _focus_dist;
+
+    update_internals();
+}
+
+Ray Camera::get_ray(float u, float v) const
+{
+    Vec3 rd = lens_radius * random_in_unit_disk(); 
+    Vec3 offset = rd.x() * cx + rd.y() * cy;
+    return Ray(origin + offset, lower_left_corner + u * horizontal + v * vertical - (origin + offset));
+}
+
+void Camera::update_internals()
+{
+    lower_left_corner = origin - focus_dist * (half_width * cx + half_height * cy + cz);
+    horizontal = 2.0 * half_width * focus_dist * cx; 
+    vertical = 2.0 * half_height * focus_dist * cy;        
+}
+
+
--- camera.h
+++ camera.h
@@ -1,63 +1,27 @@
 #pragma once
 
 #include "vec3.h"
-#include "utils.h"
+
+class Ray;
 
 class Camera
 {
 public:
-    Camera() { }
+    Camera();
 
-    Camera(float hfov, float aspect, float aperture = 1e-2, float focus_dist = 5.0f)
-    {
-        setup(hfov, aspect);
-
-        look_at(Vec3(0.0), Vec3(0, 0, -1), Vec3(0, 1, 0));
-        set_lens(aperture, focus_dist);
-    }    
-
-    void setup(float hfov, float aspect)
-    {
-        float theta = hfov * (M_PI/ 180.0);
-        half_width = fabs(tan(theta/2.0));
-        half_height = half_width / aspect;
-    
-        update_internals();
-    }
-
-    void look_at(const Vec3 &from, const Vec3 &to, const Vec3 &up)
-    {
-        origin = from;
-        cz = unit_vector(from - to);
-        cx = unit_vector(cross(up, cz));
-        cy = cross(cz, cx);
-
-        update_internals();
-    }
-
-    void set_lens(float aperture, float _focus_dist)
-    {
-        lens_radius = aperture / 2.0;
-        focus_dist = _focus_dist;
-
-        update_internals();
-    }
-
-    Ray get_ray(float u, float v)
-    {
-        Vec3 rd = lens_radius * random_in_unit_disk(); 
-        Vec3 offset = rd.x() * cx + rd.y() * cy;
-        return Ray(origin + offset, lower_left_corner + u * horizontal + v * vertical - (origin + offset));
-    }
-
+    Camera(float hfov, float aspect, float aperture = 1e-2, float focus_dist = 5.0f);
+  
+    void setup(float hfov, float aspect);
+  
+    void look_at(const Vec3 &from, const Vec3 &to, const Vec3 &up);
+  
+    void set_lens(float aperture, float _focus_dist);
+  
+    Ray get_ray(float u, float v) const;
+  
 private:
-    void update_internals()
-    {
-        lower_left_corner = origin - focus_dist * (half_width * cx + half_height * cy + cz);
-        horizontal = 2.0 * half_width * focus_dist * cx; 
-        vertical = 2.0 * half_height * focus_dist * cy;        
-    }
-
+    void update_internals();
+ 
     float half_width;
     float half_height;
     Vec3 lower_left_corner;
@@ -69,5 +33,4 @@ private:
     Vec3 cz;
     Vec3 cx;
     Vec3 cy;
-
 };
